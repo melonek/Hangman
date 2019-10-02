5e2e8ca904dadb55499938739d285fdc063bcd72
melon <meloniusz@hotmail.com>
2019-10-02 16:17:37 +0800

    js working so far

--- hangman.js
+++ hangman.js
@@ -1,5 +1,6 @@
 var keyword = "Saying to me bitch please";
 keyword = keyword.toUpperCase();
+var deadMan = 0;
 
 var length = keyword.length;
 
@@ -68,6 +69,46 @@ function start() {
   writeSaying();
 }
 
+String.prototype.pushSign = function(place, sign) {
+  if (place > this.length - 1) return this.toString();
+  else return this.substr(0, place) + sign + this.substr(place + 1);
+};
 function check(num) {
-  alert(num);
+  var guessed = false;
+
+  for (i = 0; i < length; i++) {
+    if (keyword.charAt(i) == letters[num]) {
+      keywordOne = keywordOne.pushSign(i, letters[num]);
+      guessed = true;
+    }
+  }
+  if (guessed == true) {
+    var element = "lit" + num;
+    document.getElementById(element).style.background = "#003300";
+    document.getElementById(element).style.color = "#00C000";
+    document.getElementById(element).style.border = "3px solid #00C000";
+    document.getElementById(element).style.cursor = "default";
+
+    writeSaying();
+  } else {
+    var element = "lit" + num;
+    document.getElementById(element).style.background = "#330000";
+    document.getElementById(element).style.color = "#C00000";
+    document.getElementById(element).style.border = "3px solid #C00000";
+    document.getElementById(element).style.cursor = "default";
+    document.getElementById(element).setAttribute("onclick", ";");
+
+    //dead
+    deadMan++;
+    var image = "css/images/h" + deadMan + ".jpg";
+    document.getElementById("hangman").innerHTML =
+      '<img src="' + image + '" alt=""/>';
+  }
+  //winning
+
+  if (keyword == keywordOne)
+    document.getElementById("alphabet").innerHTML =
+      "Yes, you've guessed it right: " +
+      keyword +
+      '<br><br/><span class="reset" onclick="location.reload()">ONCE AGAIN?</span>';
 }
